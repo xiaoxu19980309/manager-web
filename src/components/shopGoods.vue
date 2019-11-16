7ae0eaeccb71db949788fd64d79102107f208540
xiaoxu19980309 <1227003096@qq.com>
2019-11-16 16:30:54 +0800

    added：商品管理开始接口编写

--- src/components/shopGoods.vue
+++ src/components/shopGoods.vue
@@ -7,7 +7,7 @@
             <el-option
               v-for="(item,index) in types"
               :key="index"
-              :label="item.typename"
+              :label="item.type_name"
               :value="item.id"
             >
             </el-option>
@@ -18,7 +18,7 @@
             <el-option
               v-for="(item,index) in status"
               :key="index"
-              :label="item.typename"
+              :label="item.type_name"
               :value="item.id"
             >
             </el-option>
@@ -45,34 +45,17 @@
         stripe
         border
         style="width: 100%">
-        <el-table-column
-          prop="date"
-          label="商品名称">
+        <el-table-column prop="goods_name" label="商品名称"></el-table-column>
+        <el-table-column prop="date" label="类别"></el-table-column>
+        <el-table-column prop="gmt_create" label="创建时间"></el-table-column>
+        <el-table-column prop="name" label="状态"></el-table-column>
+        <el-table-column label="是否特价">
+          <template slot-scope="scope">
+            <span v-if="scope.row.is_special==0">否</span>
+            <span v-else>是</span>
+          </template>
         </el-table-column>
-        <el-table-column
-          prop="date"
-          label="类别">
-        </el-table-column>
-        <el-table-column
-          prop="date"
-          label="创建时间">
-        </el-table-column>
-        <el-table-column
-          prop="name"
-          label="状态">
-        </el-table-column>
-        <el-table-column
-          prop="address"
-          label="是否特价">
-        </el-table-column>
-        <el-table-column
-          prop="address"
-          label="销售统计">
-        </el-table-column>
-        <el-table-column
-          prop="address"
-          label="库存">
-        </el-table-column>
+        <el-table-column prop="goods_num" label="库存"></el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
             <div>
@@ -98,30 +81,36 @@
       :visible.sync="dialogVisible"
       width="40%"
       :before-close="handleClose">
-      <el-form>
-        <el-form-item label="商品名称" style="margin-bottom:15px">
+      <el-form label-width="90px">
+        <el-form-item label="商品名称" required style="margin-bottom:15px">
           <el-input style="margin-left:20px" v-model="form.goodName"></el-input>
         </el-form-item>
-        <el-form-item label="商品类别" style="margin-bottom:15px">
+        <el-form-item label="商品类别" required style="margin-bottom:15px">
           <el-select style="margin-left:20px" v-model="form.type" placeholder="请选择类别">
             <el-option
               v-for="(item,index) in types"
               :key="index"
-              :label="item.typename"
+              :label="item.type_name"
               :value="item.id"
             >
             </el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="商品价格" style="margin-bottom:15px">
+        <el-form-item label="商品价格" required style="margin-bottom:15px">
           <el-input style="margin-left:20px" v-model="form.price"></el-input>
         </el-form-item>
-        <el-form-item label="商品库存" style="margin-bottom:15px">
+        <el-form-item label="商品库存" required style="margin-bottom:15px">
           <el-input style="margin-left:20px" v-model="form.number"></el-input>
         </el-form-item>
+        <el-form-item label="商品介绍" style="margin-bottom:15px">
+          <el-input type="textarea" style="margin-left:20px;width:75%" v-model="form.description" :rows="3" maxlength="200"></el-input>
+        </el-form-item>
         <el-form-item label="是否特价" style="margin-bottom:15px">
-          <el-radio style="margin-left:20px" v-model="form.isSpecial" label="1">是</el-radio>
-          <el-radio v-model="form.isSpecial" label="2">否</el-radio>
+          <el-radio-group style="margin-left:20px" v-model="form.isSpecial">
+            <el-radio :label="1">是</el-radio>
+            <el-radio :label="0">否</el-radio>
+          </el-radio-group>
+          
         </el-form-item>
         <el-form-item label="上传图片" style="margin-bottom:15px">
           <el-upload
@@ -140,13 +129,14 @@
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="addGoods()">确 定</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
 <script>
 import "@/css/global.css";
+import { API } from "@/utils/api"
 export default {
   name: "shopGoods",
   data(){
@@ -156,35 +146,10 @@ export default {
       total: 0,
       title: "新增商品",
       dialogVisible: false,
-      types: [{
-        id: "1",
-        typename: "蔬菜",
-      },{
-        id: "1",
-        typename: "蔬菜",
-      }],
-      status: [{
-        id: "1",
-        typename: "蔬菜",
-      }],
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }],
-      fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
+      types: [],
+      status: [],
+      tableData: [],
+      fileList: [],
       param: {
         type: "",
         status: "",
@@ -195,20 +160,47 @@ export default {
         type: "",
         price: "",
         number: "",
-        isSpecial: 2
+        isSpecial: 0,
       }
     }
   },
   mounted(){
-
+    this.getTable()
+    this.getTypeList()
   },
   methods: {
+    getTable(param){
+      this.axios.post(API.getGoodsList,param).then(res =>{
+        if(res.resultCode!=200){
+          this.$message.error(res.resultMsg)
+        }else{
+          this.total = res.data.total
+          this.tableData = res.data.list
+        }
+      }).catch(e=>{
+
+      })
+    },
+    //获取类别列表
+    getTypeList(){
+      this.axios.post(API.getTypeList,{
+        pageSize: 999
+      }).then(res =>{
+        if(res.resultCode==200){
+          this.types = res.data.list
+        }else{
+          this.$message.error(res.resultMsg)
+        }
+      }).catch(e=>{
+
+      })
+    },
     initForm(){
       this.form.goodName = ""
       this.form.type = ""
       this.form.price = ""
       this.form.number = ""
-      this.form.isSpecial = 2
+      this.form.isSpecial = 0
     },
     editGoods(good){
       if(good==null){
@@ -218,6 +210,58 @@ export default {
         this.dialogVisible = true
         this.title = "编辑商品"
       }
+    },
+    checkNull(){
+      if(!!!this.form.goodName||this.form.goodName.trim().length==0){
+        this.$message.warning("商品名称不能为空!");
+        return false;
+      }
+      if(!!!this.form.type){
+        this.$message.warning("商品类别不能为空!");
+        return false;
+      }
+      if(!!!this.form.price){
+        this.$message.warning("商品价格不能为空!")
+        return false;
+      }
+      if(isNaN(this.form.price)){
+        this.$message.warning("商品价格只能为数字!")
+        return false;
+      }
+      if(!!!this.form.number){
+        this.$message.warning("商品库存不能为空!")
+        return false;
+      }
+      if(isNaN(this.form.number)){
+        this.$message.warning("商品库存只能为数字!")
+        return false;
+      }
+      return true;
+    },
+    //新增商品
+    addGoods(){
+      if(!this.checkNull())
+        return;
+      let param = {
+        goods_name: this.form.goodName,
+        goods_price: parseFloat(this.form.price),
+        goods_num: parseInt(this.form.number),
+        type_id: parseInt(this.form.type)
+      }
+      if(!!this.form.description){
+        param.description = this.form.description
+      }
+      this.axios.post(API.addGoods,param).then(res=>{
+        if(res.resultCode!=200){
+          this.$message.error(res.resultMsg)
+        }else{
+          this.$message.success(res.resultMsg)
+          this.getTable({pageNum: this.currentPage})
+        }
+      }).catch(e=>{
+
+      })
+
     },
     //分页
     handleCurrentChange(val){
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,14 @@
 const API = {
-  adminLogin: "common/adminLogin",
-  getUserList: "api/user/getUserList",
-  changeAdminStatus: "api/user/changeAdminStatus",
-  addGoodsType: "api/goodsType/addGoodsType",
-  getTypeList: "api/goodsType/getTypeList",
-  deleteType: "api/goodsType/deleteType",
-  updateGoodsType: "api/goodsType/updateGoodsType",
-  
+  adminLogin: "common/adminLogin",//管理员登录
+  getUserList: "api/user/getUserList",//获取用户管理列表
+  changeAdminStatus: "api/user/changeAdminStatus",//修改用户状态
+  addGoodsType: "api/goodsType/addGoodsType",//添加商品类别
+  getTypeList: "api/goodsType/getTypeList",//获取类别列表
+  deleteType: "api/goodsType/deleteType",//删除类别
+  updateGoodsType: "api/goodsType/updateGoodsType",//更新类别
+  addGoods: "api/goods/addGoods",//添加商品
+  getGoodsList: "api/goods/getGoodsList",//获取商品列表
+
 }
 
 export { API }
