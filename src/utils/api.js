7ba618c743dbb52aed531055453a93dc9c009008
xiaoxu19980309 <1227003096@qq.com>
2019-11-16 14:22:50 +0800

    added：商品类别接口地址配置

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,10 @@ const API = {
   adminLogin: "common/adminLogin",
   getUserList: "api/user/getUserList",
   changeAdminStatus: "api/user/changeAdminStatus",
+  addGoodsType: "api/goodsType/addGoodsType",
+  getTypeList: "api/goodsType/getTypeList",
+  deleteType: "api/goodsType/deleteType",
+  updateGoodsType: "api/goodsType/updateGoodsType",
   
 }
 
